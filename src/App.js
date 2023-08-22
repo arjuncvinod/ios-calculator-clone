deb5511c15ed0cc4af2072f1971cf702c2ab45b2
arjuncvinod <arjuncvinod007@gmail.com>
2023-08-22 22:08:11 +0530

    ui upadated

--- src/App.js
+++ src/App.js
@@ -8,25 +8,25 @@ function App() {
           <input type="text" />
         </form>
         <div className="keypads">
-          <button>AC</button>
-          <button>C</button>
-          <button>%</button>
-          <button>/</button>
+          <button className="controls">AC</button>
+          <button className="controls">C</button>
+          <button className="controls">%</button>
+          <button className="yellow">÷</button>
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>x</button>
+          <button className="yellow">x</button>
           <button>4</button>
           <button>5</button>
           <button>6</button>
-          <button>-</button>
+          <button className="yellow">-</button>
           <button>1</button>
           <button>2</button>
           <button>3</button>
-          <button>+</button>
+          <button className="yellow">+</button>
           <button className="zero">0</button>
           <button>.</button>
-          <button>=</button>
+          <button className="yellow">=</button>
         </div>
       </div>
     </main>
