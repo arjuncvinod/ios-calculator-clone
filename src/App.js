ab0d1e726def6d01d2be96e53056d85a2a8dfd61
arjuncvinod <arjuncvinod007@gmail.com>
2023-08-22 20:24:40 +0530

    elements added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+        <form action="">
+          <input type="text" />
+        </form>
+        <div className="keypads">
+          <button>AC</button>
+          <button>C</button>
+          <button>%</button>
+          <button>/</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>x</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button className="zero">0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
+    </main>
   );
 }
 
